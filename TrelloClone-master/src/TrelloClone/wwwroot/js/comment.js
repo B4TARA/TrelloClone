fd09fb4a18fdcdaa55befe7cb727ebb082f810e1
yantomchik <70806762+B4TARA@users.noreply.github.com>
2023-11-15 17:19:51 +0300

    init 151123

--- TrelloClone-master/src/TrelloClone/wwwroot/js/comment.js
+++ TrelloClone-master/src/TrelloClone/wwwroot/js/comment.js
@@ -19,7 +19,10 @@ function saveComment(elem) {
     const popupElement = document.querySelector(".popup");
     const cardId = popupElement.getAttribute('cardId');
 
+
     if (commentAreaElem.value != "") {
+
+        createElementComment(commentAreaElem)
 
         var url = "/Card/AddComment";
         formData = new FormData();
@@ -49,5 +52,51 @@ function saveComment(elem) {
             }
         });
     }
+    commentAreaElem.value = ""
 }
 
+function createElementComment(elem) {
+    const commentAreaMainWrapper = document.getElementById('comment_popup_card_wrapper');
+    const imgUserPath = document.querySelector('.profile-content-image');
+    const userName = document.querySelector('.profile_name')
+    let div = document.createElement('div')
+    div.classList.add('comment_user_wrapper')
+
+    div.innerHTML = `
+                            <div class="first_row_grid_description margin_container_top_middle">
+
+
+                                <div class="card_holders_wrapper">
+                                    <div class="holder_image_wrapper">
+
+
+                                        <img src="${imgUserPath.src}" alt="holder_image">
+
+
+                                    </div>
+                                </div>
+
+
+                                <div class="mid_title">
+                                    ${userName.innerText}
+                                </div>
+                            </div>
+
+
+                            <div class="second_row_grid_description" id="description">
+                                <div></div>
+
+
+                                <div class="commentarea_wrapper" id="commentarea_wrapper">
+                                    <div class="comment_area">
+
+
+                                        <div class="mid_description">${elem.value}</div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+    `
+        commentAreaMainWrapper.appendChild(div)
+}
